64942825d77210ef3cac71b26bb5071e3d4f37c9
Nathan Holt <nxh7119@rit.edu>
2019-02-24 21:11:44 -0500

    talwejt

--- postdeploy.php
+++ postdeploy.php
@@ -1,9 +1,17 @@
 <?php
+	$secret = "SecretWebhookKey18593";
+	$hashed_secret = sha1($secret);
 	$post_data = file_get_contents('php://input');
 	
 	if (!empty($post_data)) {
 		echo "We're here!";
 		shell_exec('cd ../ && git pull');
+		$hashed_received = $_HEADER['X-Hub-Signature'];
+		if ($hashed_received == $hashed_secret) {
+			echo "SECRETS MATCH";
+		} else{
+			echo "do not match";
+		}
 		echo "pulled";
 		//mail('nathanwayneholt@gmail.com', 'Auto-Deploy Activated', 'nathanwayneholt.com has auto-updated due to the detection that the remote repo has been updated.')
 	}
