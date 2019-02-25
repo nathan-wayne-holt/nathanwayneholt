c0b7162df8065edfa296f3ad5e7f6f9fd07bf724
Nathan Holt <nxh7119@rit.edu>
2019-02-24 19:39:57 -0500

    Changed postdeploy

--- postdeploy.php
+++ postdeploy.php
@@ -1,5 +1,7 @@
 <?php
 	if (isset($_POST['payload'])) {
+		$message = "Received a payload. Did you make a push request?";
+		mail("nathanwayneholt@gmail.com", "Push Received", $message);
 		shell_exec('cd ../ && git pull');
 	} else {
 		echo "No payload was sent.";
