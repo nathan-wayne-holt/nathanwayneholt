ceac5e314fcce043fe3540b92e224baaa5684083
Nathan Holt <nxh7119@rit.edu>
2019-02-25 18:38:36 -0500

    testing

--- postdeploy.php
+++ postdeploy.php
@@ -2,8 +2,10 @@
 	$secret = "SecretWebhookKey18593";
 	
 	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+		echo "NOT SET";
 		throw new Exception('X-Hub-Signature missing.')
 	} elseif (!extension_loaded('hash')) {
+		echo "HASH NOT AVAILABLE";
 		throw new Exception('Hash extension not loaded.')
 	}
 	
