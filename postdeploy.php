525f067aa6efaf85041b445843807e4cd695daa3
Nathan Holt <nxh7119@rit.edu>
2019-02-24 19:46:48 -0500

    testing

--- postdeploy.php
+++ postdeploy.php
@@ -1,5 +1,6 @@
 <?php
-	if (isset($_POST['payload'])) {
+	if (isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+		echo $_SERVER['HTTP_X_HUB_SIGNATURE'];
 		$message = "Received a payload. Did you make a push request?";
 		mail("nathanwayneholt@gmail.com", "Push Received", $message);
 		shell_exec('cd ../ && git pull');
