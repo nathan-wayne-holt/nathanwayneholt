3992da9be51b7baf77add1a9c86386800ae657a6
Nathan Holt <nxh7119@rit.edu>
2019-02-25 18:40:04 -0500

    testing

--- postdeploy.php
+++ postdeploy.php
@@ -3,10 +3,10 @@
 	
 	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
 		echo "NOT SET";
-		throw new Exception('X-Hub-Signature missing.')
+		//throw new Exception('X-Hub-Signature missing.')
 	} elseif (!extension_loaded('hash')) {
 		echo "HASH NOT AVAILABLE";
-		throw new Exception('Hash extension not loaded.')
+		//throw new Exception('Hash extension not loaded.')
 	}
 	
 	list($algorithm, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
@@ -14,7 +14,8 @@
 	$rawData = file_get_contents('php://input')
 	
 	if ($hash !== hash_hmac($algorithm, $rawData, $secret)) {
-		throw new Exception('Secret does not match hash.')
+		echo "problem hashsing";
+		//throw new Exception('Secret does not match hash.')
 	}
 	
 	#Successfully passed secret into the header. Now run git pull.
