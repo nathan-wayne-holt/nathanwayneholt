faa9e4ca51c58ed564a655c13344a6f59068ed19
Nathan Holt <nxh7119@rit.edu>
2019-02-24 21:07:29 -0500

    test

--- postdeploy.php
+++ postdeploy.php
@@ -1,10 +1,10 @@
 <?php
-	print_r($_POST);
 	$post_data = file_get_contents('php://input');
 	
 	if (!empty($post_data)) {
 		echo "We're here!";
-		//shell_exec('cd ../ && git pull');
+		shell_exec('cd ../ && git pull');
+		echo "pulled";
 		//mail('nathanwayneholt@gmail.com', 'Auto-Deploy Activated', 'nathanwayneholt.com has auto-updated due to the detection that the remote repo has been updated.')
 	}
 ?>
