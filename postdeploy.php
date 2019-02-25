5a1d16fb36dce42bc6a4f64b022b48803716d0a4
Nathan Holt <nxh7119@rit.edu>
2019-02-24 20:58:27 -0500

    postdeploy working MVP?

--- postdeploy.php
+++ postdeploy.php
@@ -1,5 +1,8 @@
 <?php
 	$post_data = file_get_contents('php://input');
-	echo $post_data;
-	shell_exec('cd ../ && git pull');
+	
+	if (!empty($post_data)) {
+		shell_exec('cd ../ && git pull');
+		mail('nathanwayneholt@gmail.com', 'Auto-Deploy Activated', 'nathanwayneholt.com has auto-updated due to the detection that the remote repo has been updated.')
+	}
 ?>
